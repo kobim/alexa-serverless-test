01954682c4c62bd399cc6b8444f4a635c394aa1e
Kobi Meirson <kobi@right.co.il>
2018-02-01 14:39:45 +0200

    New serverless-azure-functions allows packaging with external files

--- azure/handler.js
+++ azure/handler.js
@@ -1,24 +1,5 @@
 'use strict';
-const alexa = require('alexa-app');
-
-const alexaApp = new alexa.app('sls-test');
-
-alexaApp.launch((request, response) => {
-  response.say(`Serverless with Azure works!`);
-});
-
-alexaApp.intent('sayNumber', {
-    'slots': {
-      'number': 'AMAZON.NUMBER'
-    },
-    'utterances': ['say the number {-|number}']
-  }, (request, response) => {
-    const number = request.slot('number');
-    if (!number) {
-      return response.say('What number?').shouldEndSession(false);
-    }
-    response.say(`You said number ${number}`);
-});
+const alexaApp = require('./sharedApp')('Azure');
 
 exports.alexa = (context, request) => {
   alexaApp.request(request.body)
